1e5a076cef7853b94c78e0f9a02c7b1cf68fba8e
Yash tambade <tambadeyash96@gmail.com>
2026-06-10 02:28:24 +0530

    Update AnswerButton.jsx

--- src/components/quiz/AnswerButton.jsx
+++ src/components/quiz/AnswerButton.jsx
@@ -9,7 +9,7 @@ const AnswerButton = memo(function AnswerButton({ index, text, onClick, disabled
   ].filter(Boolean).join(' ');
 
   return (
-    <button className={cls} onClick={onClick} disabled={disabled}>
+    <button className={cls} onClick={(e) => onClick && onClick(e)} disabled={disabled}>
       <div className="ans-shape">{SHAPES[index]}</div>
       <span className="ans-text">{text}</span>
     </button>
